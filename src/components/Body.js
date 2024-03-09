68a858827a5cc849b2d5fef8ef5be41ccc5b4389
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-03-09 14:59:05 +0200

    added body component file and imported it inside of app.js

---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Body = () => {
+  return (
+    <div>
+      <p>the notes app body --body.js--</p>
+    </div>
+  )
+}
+
+export default Body
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
 
     return(
         <div>
-            <h1>Sick react notes app</h1>
+            <h1>Sick react notes app --header.js--</h1>
         </div>
     )
 }
