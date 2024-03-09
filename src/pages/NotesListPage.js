ed14b1cfd975010740f5f46d0c5ed8b9b5441066
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-03-09 15:22:27 +0200

    added assets folder with dummy static data, mapped through the dummy data inside of NotesListpage to pull each record and display it as a list item

--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import notes from '../assets/data'
 
 const NotesListPage = () => {
   return (
     <div className='NotesListPage'>
-      <p>notes list feed --NotesListPage.js--</p>
+        <p>notes list feed --NotesListPage.js--</p>
+        <ul className='notes-list'>
+            {/* for note in notes */}
+            {notes.map(note => (
+                <li>{note.body}</li>
+            ))}
+        </ul>
     </div>
   )
 }
