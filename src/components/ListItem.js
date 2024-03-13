e2ae5656bbad88497fc6b8deab4008c0ea8b860c
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-03-13 20:39:30 +0200

    added multiple styling functions that display a title, a shortbody and the date and time of the note

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+let getTitle = (note) => {
+  const title = note.body.split('\n')[0]
+
+  if (title.length >= 50){
+    return title.slice(0,50)+'...'
+  }
+  return title
+}
+
+let getDate = (note) => {
+  return new Date(note.updated).toLocaleDateString()
+}
+
+let getShortBody = (note) => {
+  const shortBody =  note.body.split('\n').slice(1).join('').slice(0,50)
+  
+  if (shortBody.length >= 50){
+    return shortBody.slice(0,50)+'...'
+  }
+  return shortBody
+}
+
+let getNoteBody
 //props passes immutable object()
 const ListItem = ({note}) => {
   return (
     <li className='ListItem'>
       <div className='notes-list-item'>
         <Link to={`/note/${note.id}`}>
-          <h3>{note.body}</h3>
+          <h3>{getTitle(note)}</h3>
+          {getShortBody(note) !== '' && (
+            <p><span>{getShortBody(note)}</span></p>
+          )}
+          <p><span>{getDate(note)} {note.updated.split('T')[1].slice(0,-5)}</span></p>
         </Link>
       </div>
     </li>
